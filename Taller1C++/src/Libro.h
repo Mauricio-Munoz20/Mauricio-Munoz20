e0699ec8ba4d56205788ea41f159fd193777faaa
Mauricio-Munoz20 <mauricio.munoz01@alumnos.ucn.cl>
2024-10-13 23:26:46 -0300

    Update Libro.h

--- Taller1C++/src/Libro.h
+++ Taller1C++/src/Libro.h
@@ -10,23 +10,62 @@
 #define LIBRO_H_
 #include "MaterialBibliografico.h"
 
+/**
+ * @brief Clase que representa un libro, hereda de MaterialBibliografico.
+ * 
+ * La clase Libro extiende la funcionalidad de MaterialBibliografico añadiendo
+ * atributos específicos de los libros, como la fecha de publicación y un resumen.
+ */
 class Libro : public MaterialBibliografico {
 private:
-	std::string fechaPublicación;
-	std::string resumen;
+	std::string fechaPublicación; /**< Fecha de publicación del libro. */
+	std::string resumen; /**< Resumen del contenido del libro. */
 public:
+	/**
+     	* @brief Constructor de la clase Libro.
+     	* @param nombre Nombre del libro.
+     	* @param isbn ISBN del libro.
+     	* @param autor Autor del libro.
+     	* @param fechaPublicación Fecha de publicación del libro.
+     	* @param resumen Resumen del libro.
+     	*/
 	Libro(std::string nombre, std::string isbn, std::string autor, std::string fechaPublicación, std::string resumen);
+	/**
+     	* @brief Muestra la información del libro.
+     	* 
+     	* Este método anula la función virtual pura de la clase base para
+     	* proporcionar una visualización específica de los detalles del libro.
+     	*/
 	void mostrarInformación() const override;
+	/**
+     	* @brief Destructor de la clase Libro.
+     	*/
 	virtual ~Libro();
+	/**
+     	* @brief Obtiene la fecha de publicación del libro.
+     	* @return Una referencia constante a la fecha de publicación.
+     	*/
 	const std::string& getFechaPublicación() const {
 		return fechaPublicación;
 	}
+	/**
+     	* @brief Establece la fecha de publicación del libro.
+     	* @param fechaPublicación La nueva fecha de publicación.
+     	*/
 	void setFechaPublicación(const std::string &fechaPublicación) {
 		this->fechaPublicación = fechaPublicación;
 	}
+	/**
+     	* @brief Obtiene el resumen del libro.
+     	* @return Una referencia constante al resumen.
+     	*/
 	const std::string& getResumen() const {
 		return resumen;
 	}
+	/**
+     	* @brief Establece el resumen del libro.
+     	* @param resumen El nuevo resumen.
+     	*/
 	void setResumen(const std::string &resumen) {
 		this->resumen = resumen;
 	}
